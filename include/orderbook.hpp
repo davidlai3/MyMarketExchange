cb8e0727bf2ea80450b465a6e870c23e2c1553be
David <lairdavid1@gmail.com>
2025-09-30 13:54:48 -0400

    Updated underlying orderbook container types.

--- include/orderbook.hpp
+++ include/orderbook.hpp
@@ -4,7 +4,9 @@
 
 #include <map>
 #include <deque>
+#include <memory>
 #include <unordered_map>
+#include <optional>
 
 class OrderBook {
     public:
@@ -12,7 +14,7 @@ class OrderBook {
          * price-time priority.
          * @param order The order to be added.
          * @param logger Logger instance for logging actions. */
-        void add_order(Order &order, Logger &logger);
+        void add_order(std::shared_ptr<Order>&, Logger &logger);
 
         /* Cancels an order by its ID.
          * @param orderID The ID of the order to be canceled.
@@ -21,18 +23,34 @@ class OrderBook {
 
         /* Prints the current state of the order book, showing bids and asks. */
         void print_book() const;
+
+        /* Get total quantity at a specific price level for a given side
+         * @param symbol The stock symbol.
+         * @param side The side (BUY or SELL).
+         * @param price The price level to check.
+         * @return Total quantity at the specified price level.
+         */
+        int32_t get_total_quantity_at(const char* symbol, Side side, double price) const;
+
+        /* Get the best bid and ask orders for a given symbol
+         * @param symbol The stock symbol.
+         * @return The best bid and ask orders.
+         */
+        std::optional<std::weak_ptr<const Order>> best_bid(const char* symbol) const;
+        std::optional<std::weak_ptr<const Order>> best_ask(const char* symbol) const;
+
     private:
         // Data structures to hold bids and asks
-        std::map<double, std::deque<Order>, std::greater<>> bids; // highest price first
-        std::map<double, std::deque<Order>> asks;                 // lowest price first
+        std::map<double, std::deque<std::shared_ptr<Order>>, std::greater<>> bids; // highest price first
+        std::map<double, std::deque<std::shared_ptr<Order>>> asks;                 // lowest price first
 
         // Map to track orders by ID for quick access
-        std::unordered_map<OrderID, Order> orderMap;
+        std::unordered_map<OrderID, std::weak_ptr<Order>> orderMap;
 
         /* Match incoming orders with existing ones
-         * @param incoming The incoming order to match.
+         * @param incoming shared_ptr to incoming order to match.
          * @param logger Logger instance for logging actions.
          * @param book book to search over (either bids or asks)
          */
-        template<typename BookType> void match(Order &incoming, Logger &logger, BookType& book);
+        template<typename BookType> void match(std::shared_ptr<Order>& incoming, Logger &logger, BookType& book);
 };
